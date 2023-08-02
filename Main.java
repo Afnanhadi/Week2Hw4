5baea04baf43c3c78c27234d862ff235d69626ce
Afnanhadi <140561556+Afnanhadi@users.noreply.github.com>
2023-08-03 02:38:05 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
+
+
 public class Main {
     public static void main(String[] args) {
+        Movie movie=new Movie("I Am Legend",50,"Francis Lawrence");
+       System.out.print("Movie price after discount:  "+movie.getDiscount()+"\n");
+       System.out.println("-------------------------------");
+        Book book= new Book("NARUTO",90,"Masashi Kishimoto");
+       System.out.println(book.toString());
+        System.out.println("Book price after discount:  "+book.getDiscount());
 
-        MovablePoint movablePoint=new MovablePoint(4,7,5,3);
-        movablePoint.moveLeft();
-      movablePoint.moveRight();
-        movablePoint.moveUp();
-      movablePoint.moveDown();
+
+
     }
 }
